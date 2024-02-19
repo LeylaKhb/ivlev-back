0ed6dfa0ef9235b16bac78e848144c341bdbb8f6
LeylaKhb <leyla.khbi@gmail.com>
2024-02-19 12:18:24 +0300

    Added jwt response

--- src/main/java/ivlev/ivlevback/config/SecurityConfig.java
+++ src/main/java/ivlev/ivlevback/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package ivlev.ivlevback.config;
 
 import ivlev.ivlevback.service.PersonDetailsService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,10 +24,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@EnableJpaRepositories("ivlev.ivlevback.repositories")
 public class SecurityConfig {
     private final PersonDetailsService personDetailsService;
+    private final JWTFilter jwtFilter;
 
     @Autowired
-    public SecurityConfig(PersonDetailsService personDetailsService) {
+    public SecurityConfig(PersonDetailsService personDetailsService, JWTFilter jwtFilter) {
         this.personDetailsService = personDetailsService;
+        this.jwtFilter = jwtFilter;
     }
 
     @Bean
@@ -32,7 +37,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/admin").hasRole("ADMIN")
                 .requestMatchers("/", "/login", "/registration").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
         ).formLogin(formLogin -> formLogin
                 .loginPage("/login")
                 .loginProcessingUrl("/api/login")
@@ -43,7 +48,10 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/login")
 //        ).csrf(csrf -> csrf
 //                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
-        ).csrf(csrf -> csrf.disable());
+        ).csrf(csrf -> csrf.disable()
+        ).sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -55,6 +63,11 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return personDetailsService;
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
     @Bean
     public WebMvcConfigurer corsConfigurer() {
--- src/main/java/ivlev/ivlevback/controllers/PeopleController.java
+++ src/main/java/ivlev/ivlevback/controllers/PeopleController.java
@@ -1,6 +1,10 @@
 package ivlev.ivlevback.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ivlev.ivlevback.dto.PersonDTO;
+import ivlev.ivlevback.service.JWTUtil;
 import jakarta.servlet.http.HttpServletResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -9,32 +13,45 @@ import ivlev.ivlevback.models.Person;
 import ivlev.ivlevback.service.RegistrationService;
 import ivlev.ivlevback.validator.PersonValidator;
 
+import java.util.Map;
+
 @RestController
 public class PeopleController {
     private final RegistrationService registrationService;
     private final PersonValidator personValidator;
 
+    private final JWTUtil jwtUtil;
+    private final ModelMapper modelMapper;
+
     @Autowired
-    public PeopleController(RegistrationService registrationService, PersonValidator personValidator) {
+    public PeopleController(RegistrationService registrationService, PersonValidator personValidator, JWTUtil jwtUtil, ModelMapper modelMapper) {
         this.registrationService = registrationService;
         this.personValidator = personValidator;
+        this.jwtUtil = jwtUtil;
+        this.modelMapper = modelMapper;
     }
 
-    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+//    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @PostMapping("/registration")
-    public Person registration(@RequestBody Person person,
+    public String registration(@RequestBody PersonDTO personDTO,
                                BindingResult bindingResult,
                                HttpServletResponse httpServletResponse) {
+        Person person = convertToPerson(personDTO);
         System.out.println(person);
         personValidator.validate(person, bindingResult);
 
-//        if (bindingResult.hasErrors())
-//            return ;
+        if (bindingResult.hasErrors())
+            return "Пользователь с такой почтой уже существует";
 
 
         registrationService.register(person);
+        String token = jwtUtil.generateToken(person.getEmail());
+        return token;
 
-        return person;
+    }
+
+    private Person convertToPerson(PersonDTO personDTO) {
+        return modelMapper.map(personDTO, Person.class);
     }
 
     @GetMapping("/")
